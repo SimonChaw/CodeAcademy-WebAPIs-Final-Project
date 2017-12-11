ac507d3305e937a3cf9fff19ca2c29b6a57610b9
SimonChaw <chawblah@gmail.com>
2017-12-11 17:35:10 -0400

    Inital setup

--- api/employees.js
+++ api/employees.js
@@ -4,7 +4,6 @@ const timesheetsRouter = require('./timesheets');
 
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
-let employeeT;
 /*-------
 EMPLOYEE ROUTER
 --------*/
